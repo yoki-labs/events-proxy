491d2723b0105fdc836a70e91397eb76a23610c3
Skillz <gankstarsleague@gmail.com>
2022-05-03 15:57:03 -0400

    feat: basic concept example

---
+++ gateway.ts
@@ -0,0 +1,16 @@
+import { WebSocketManager } from "guilded.js";
+
+export function spawnGateway(gatewayOptions: GatewayOptions) {
+  // TODO: Spawn a gateway with the provided options
+  const gateway = new WebSocketManager(gatewayOptions);
+
+  gateway.socket.on("message", async (data) => {
+    await fetch(gatewayOptions.userEndpointUrl, {
+      body: JSON.stringify(data),
+    });
+  });
+}
+
+export interface GatewayOptions extends WebSocketOptions {
+  userEndpointUrl: string;
+}
